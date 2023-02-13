b2525e8b29e43cb03c1e70f99c13fee464b9c917
DavidMoralesDeveloper <david.morales.developer.01@gmail.com>
2023-02-13 12:34:39 -0600

    'creando carpetas de  rutas'
    creando carpetas y primeras rutas de ejemplo modularizadas , para segir trabajando en ellas

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import server from './src/server.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-console.log('combio de david prueba de git craken')
 const PORT = process.env.PORT || 5000;
 
 (async function startServer(){
