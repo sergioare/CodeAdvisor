183e4fa0ada6ddacf5485eac0dcd9da01e7106ea
María Teresa Morón <mariatmd97@hotmail.com>
2023-02-15 17:49:07 -0500

    feature(client/components): add component Comercial and Testimonials, modify Home/Cards

---
+++ client/src/components/Commercial/Commercial.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Commercial.scss'
+
+export const Commercial = () => {
+  return (
+    <div className='ContPrincipal'>
+      <div className='textCont'>
+        <h1>Teaching in the internet age </h1>
+        <h1>means we must teach <p>tomorrow's</p></h1>
+        <h1><span>skills today</span></h1>
+      </div>
+      <div className='buttonCont'>
+        <button>Join as Advisor</button>
+        <button>Join as Student</button>
+      </div>
+      
+    </div>
+  )
+}
