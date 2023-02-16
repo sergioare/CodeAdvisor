dcb5f15287a39144f1989998110f5bdadc441d8d
nicobise <nicobise@hotmail.com>
2023-02-16 13:59:17 -0300

    App.js asegurando elemento

--- client/src/App.js
+++ client/src/App.js
@@ -1,2 +1,27 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { Landing, Home } from './Views/index';
+import AboutUs from "./components/AboutUs/AboutUs"
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  const location = useLocation();
+
+  const [isConfigBarOpen, setIsConfigBarOpen] = useState(false);
+
+  const toggleConfigBar = () => {
+    setIsConfigBarOpen(!isConfigBarOpen);
+  };
+  return (
+    <div className="App">
+    {location.pathname === '/home' && <Navbar toggleConfigBar={toggleConfigBar} />}
+    {isConfigBarOpen && <ConfigSideBar />}
+    <Routes>
+      <Route exact path='/' element={<Landing />} />
+      <Route path='/home' element={<Home toggleConfigBar={toggleConfigBar} isConfigBarOpen={isConfigBarOpen} />} />
+      <Route path='/about' element={<AboutUs />} />
+    </Routes>
+  </div>
+  );
+}
 
 export default App;
