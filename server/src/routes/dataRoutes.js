79d415200577f3581ceed455b008b77b157e433c
templo20blanco10gh <angel.villamizar.p@gmail.com>
2023-02-16 17:57:04 -0500

    get user

--- server/src/routes/dataRoutes.js
+++ server/src/routes/dataRoutes.js
@@ -6,7 +6,13 @@ const {
 
     getReviewrs,
     addReviewrs,
-    updateReviewrs
+    updateReviewrs,
+
+    getLenguajes,
+    addLenguajes,
+    updateLenguajes,
+
+    getSpecialty
     } = require('../controllers/dataController');
 
 const dataRoute = Router();
@@ -18,6 +24,12 @@ dataRoute.put('/autores/:id',updateAutor);
 dataRoute.get('/reviewrs/',getReviewrs);
 dataRoute.post('/reviewrs/',addReviewrs);
 dataRoute.put('/reviewrs/:id',updateReviewrs);
+
+dataRoute.get('/lenguajes/',getLenguajes);
+dataRoute.post('/lenguajes/',addLenguajes);
+dataRoute.put('/lenguajes/:id',updateLenguajes);
+
+dataRoute.get('/Specialty/',getSpecialty);
 
 
 module.exports = dataRoute;
