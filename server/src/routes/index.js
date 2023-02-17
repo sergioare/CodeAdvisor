feff56ade20fc6363b5082997c69657d5e758a92
CodigoJaguar <israel.san_mendoza@hotmail.com>
2023-02-17 17:40:36 -0600

    feat(src/Autentification)

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,15 +7,7 @@ const dataRoute = require('./dataRoutes')
 const getAllUsers  = require('../controllers/usersController');
 
 
-<<<<<<< HEAD
-
-const router = express.Router();
-
-router.get("/users", getAllUsers);
-
-=======
 router.get("/users", getAllUsers)
->>>>>>> ade708211bfcec29742ccb751c183e6ed26be972
 router.use("/user", userRoute);
 router.use('/data', dataRoute)
 
