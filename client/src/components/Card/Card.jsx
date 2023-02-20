03d791ec71ed1c98c4e04b2eb4f6143cea4aa788
nicobise <nicobise@hotmail.com>
2023-02-20 18:15:30 -0300

    popUps

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -12,7 +12,7 @@ const CardGeneral = (props) => {
           <div className="letra">
             <h2>Tech Skills:</h2> <p>{props.TechSkills?.length > 1 ? props.TechSkills.join(', ') : props.TechSkills}</p>
             <h2>Specialty:</h2> <p>{props.Specialty?.length > 1 ? props.Specialty.join(', ') : props.Specialty}</p>
-            <h2>Ranking:</h2> <p>⭐{props.Score}</p>
+            <h2>Score:</h2> <p>⭐{props.Score}</p>
           </div>
           <Link to={`/user/${props.id}`}><button variant="primary">Meet Advisor</button></Link>
         </div>
