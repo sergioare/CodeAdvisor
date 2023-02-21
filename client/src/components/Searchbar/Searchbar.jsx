98dacc78203a8935a86e45f91c447834938dbe82
María Teresa Morón <mariatmd97@hotmail.com>
2023-02-21 10:23:02 -0500

    refactor(src/components):refactor non-functional buttons

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,25 +1,34 @@
-import  './Searchbar.scss'
-
+import './Searchbar.scss'
+import Swal from 'sweetalert2'
 
 const Searchbar = () => {
-    return (
-        <div className='searchbar'>
-          <input
-            className='input'
-            autoComplete="off"
-            type="text"
-            placeholder="Search..."
-            id="search"
-          />
-          
-          <button
-          className='btn'
-          type="submit"
-          >
-         <i className="fa-sharp fa-solid fa-magnifying-glass"></i>
-        </button>
-        </div>
-    );
+  const showAlert = () => {
+    Swal.fire({
+      title: "Sorry, We are working for you",
+      icon: "warning",
+      footer: "<b>Continue to enjoy our services</b>",
+      timer: 3000,
+    })
+  }
+  return (
+    <div className='searchbar'>
+      <input
+        className='input'
+        autoComplete="off"
+        type="text"
+        placeholder="Search..."
+        id="search"
+      />
+
+      <button
+        className='btn'
+        type="submit"
+        onClick={showAlert}
+      >
+        <i className="fa-sharp fa-solid fa-magnifying-glass"></i>
+      </button>
+    </div>
+  );
 };
 
 export default Searchbar;
