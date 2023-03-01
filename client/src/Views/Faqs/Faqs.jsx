24a3474a8501528be621a64d67b2861d3cdc5b7e
Sergio Arévalo <sergioarevalo301@gmail.com>
2023-02-28 23:19:05 -0500

    feat(src/views/faqs): update code to open an especific state

--- client/src/Views/Faqs/Faqs.jsx
+++ client/src/Views/Faqs/Faqs.jsx
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 const Faqs = () => {
-    const [isOpen,setIsOpen]=useState(false);
+    const [isOpen,setIsOpen]=useState(
+        { 0: false, 1: false, 2: false, 3: false, 4: false }
+    );
+    
     return (
         <div className='faqs'>
             <div className="wrapper">
@@ -30,12 +33,13 @@ const Faqs = () => {
                         <div className='container-QA' key={index}>
                             <div className="question">
                                 <h3>{item.question}
-                                <i key={index} onClick={()=> setIsOpen(!isOpen)} className="plus fa-solid fa-plus"></i>
+                                <i  onClick={() => setIsOpen(prevState => ({ ...prevState, [index]: !prevState[index] }))}
+                                 className={`plus fa-solid fa-plus ${isOpen[index] ? 'is-open' : ''}`}></i>
                                 </h3>
                                 
                             </div>
 
-                            <div className={`answer ${isOpen && "is-open"}`} > 
+                            <div className={`answer ${isOpen[index] ? 'is-open' : ''}`} > 
                                 <p>{item.answer}</p>
                             </div> 
 
