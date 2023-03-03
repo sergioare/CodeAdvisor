f1b04962fe0b7944bd05687d6cc3e660c5a2c4e4
María Teresa Morón <mariatmd97@hotmail.com>
2023-03-02 22:03:26 -0500

    fix(src/components) actions/reviews

--- server/src/controllers/advisorsController.js
+++ server/src/controllers/advisorsController.js
@@ -45,30 +45,30 @@ const getAllAdvisors = async (req, res, next) => {
                 if (doc.data().score && doc.data().score.length > 1) {
                     let s = 0
                     score = doc.data().score
-                    score.map(x => s = s + x )
+                    score.map(x => s = s + x)
                     score = [s / score.length]
                 }
                 const advisor = new Advisors(
                     doc.id,
-                    doc.data().Nickname     || "empty",
-                    doc.data().Firstname    || "empty",
-                    doc.data().Lastname     || "empty",
-                    doc.data().Contact      || "00-000-00-00",
-                    doc.data().Img          || "https://img.freepik.com/vector-premium/fondo-pagina-error-404-distorsion_23-2148086293.jpg?w=2000",
-                    doc.data().Residence    || "empty",
-                    doc.data().Language     || "empty",
-                    doc.data().Price        || "000",
-                    score                   || doc.data().score,
-                    doc.data().About        || "empty",
-                    doc.data().Specialty    || ["empty"],
-                    doc.data().TechSkills   || ["empty"],
-                    );                                 
-                    if (doc.data().status === true) {
-                        advisors.push(advisor);
-                    }
-                });
-                res.send(advisors);
-            }
+                    doc.data().Nickname || "empty",
+                    doc.data().Firstname || "empty",
+                    doc.data().Lastname || "empty",
+                    doc.data().Contact || "00-000-00-00",
+                    doc.data().Img || "https://img.freepik.com/vector-premium/fondo-pagina-error-404-distorsion_23-2148086293.jpg?w=2000",
+                    doc.data().Residence || "empty",
+                    doc.data().Language || "empty",
+                    doc.data().Price || "000",
+                    score || doc.data().score,
+                    doc.data().About || "empty",
+                    doc.data().Specialty || ["empty"],
+                    doc.data().TechSkills || ["empty"],
+                );
+                if (doc.data().status === true) {
+                    advisors.push(advisor);
+                }
+            });
+            res.send(advisors);
+        }
     } catch (error) {
         res.status(400).send(error.message);
     }
@@ -83,32 +83,32 @@ const getIdAdvisors = async (req, res, next) => {
         if (!data.exists) {
             res.status(404).send(`Advisor with id: ${id}. Does not exist`);
         } else {
-            let reviwers = ["empty",0]
+            let reviwers = ["empty", 0]
             let schedules = ["empty"]
-            if(data.data().statusReviwers === true) {
-                reviwers = await getAllReviews(data.id)
+            if (data.data().statusReviwers === true) {
+                reviwers = await getAllReviews(id)
             }
             if (data.data().statusSchedules === true) {
                 schedules = await getAllSchedules(data.id)
             }
             const advisors = new Advisors(
                 data.id,
-                data.data().Nickname    || "empty",   
-                data.data().Firstname   || "empty",  
-                data.data().Lastname    || "empty",   
-                data.data().Contact     || "00-000-00-00",    
-                data.data().Img         || "https://img.freepik.com/vector-premium/fondo-pagina-error-404-distorsion_23-2148086293.jpg?w=2000",        
-                data.data().Residence   || "empty",  
-                data.data().Language    || ["empty"],   
-                data.data().Price       || "000",      
-                reviwers[1]             ,                  
-                data.data().About       || "empty",      
-                data.data().Specialty   || ["empty"],  
-                data.data().TechSkills  || ["empty"], 
-                reviwers[0]             ,
+                data.data().Nickname || "empty",
+                data.data().Firstname || "empty",
+                data.data().Lastname || "empty",
+                data.data().Contact || "00-000-00-00",
+                data.data().Img || "https://img.freepik.com/vector-premium/fondo-pagina-error-404-distorsion_23-2148086293.jpg?w=2000",
+                data.data().Residence || "empty",
+                data.data().Language || ["empty"],
+                data.data().Price || "000",
+                reviwers[1],
+                data.data().About || "empty",
+                data.data().Specialty || ["empty"],
+                data.data().TechSkills || ["empty"],
+                reviwers[0],
                 schedules
-                );
-                res.send(advisors);
+            );
+            res.send(advisors);
         }
     } catch (error) {
         res.status(400).send(error.message);
@@ -120,6 +120,7 @@ const getIdAdvisors = async (req, res, next) => {
 const getAdvisorsAllReviwers = async (req, res, next) => {
     console.log("get:Advisors_All_Reviews");
     const id = req.params.id
+    console.log(req.params)
     const reviwers = [];
     try {
         const fire = await firestore.collection(`/Advisors/${id}/Reviwers`);
@@ -130,17 +131,17 @@ const getAdvisorsAllReviwers = async (req, res, next) => {
             data.forEach((doc) => {
                 const r = new Reviwers(
                     doc.id,
-                    doc.data().Name     || "empty",
-                    doc.data().Img      || "https://img.freepik.com/vector-premium/fondo-pagina-error-404-distorsion_23-2148086293.jpg?w=2000",
-                    doc.data().Reviwer  || "empty",
-                    doc.data().score    || 0
-                    );
-                    if (doc.data().status === true) {  
-                        reviwers.push(r);
-                    }
-                });
-                res.send(reviwers);
-            }
+                    doc.data().Name || "empty",
+                    doc.data().Img || "https://img.freepik.com/vector-premium/fondo-pagina-error-404-distorsion_23-2148086293.jpg?w=2000",
+                    doc.data().Reviwer || "empty",
+                    doc.data().score || 0
+                );
+                if (doc.data().status === true) {
+                    reviwers.push(r);
+                }
+            });
+            res.send(reviwers);
+        }
     } catch (error) {
         res.status(400).send(error.message);
     }
@@ -148,19 +149,19 @@ const getAdvisorsAllReviwers = async (req, res, next) => {
 
 const addAdvisorsReviwers = async (req, res, next) => {
     console.log("Advisor_add_Reviwer");
-    const id                = req.params.id
-    const reviewdata        = req.body;
-    const reviewScore       = req.body.score || 1
-    const statusReviwers    = true
-    reviewdata.status       = true
+    const id = req.params.id
+    const reviewdata = req.body;
+    const reviewScore = req.body.score || 1
+    const statusReviwers = true
+    reviewdata.status = true
     try {
         await firestore.collection(`/Advisors/${id}/Reviwers`).add(reviewdata);
         const fire = await firestore.collection("Advisors").doc(id);
         const data = await fire.get()
         const score = data.data().score || []
         score.push(reviewScore)
-        await fire.update({score});
-        await fire.update({statusReviwers});
+        await fire.update({ score });
+        await fire.update({ statusReviwers });
         res.send("Reviews successfuly");
     } catch (error) {
         res.status(400).send(error.message);
@@ -194,34 +195,34 @@ const getAdvisorsAllSchedules = async (req, res, next) => {
         } else {
             data.forEach((doc) => {
                 const schedule = new Schedules(
-                    doc.id, 
-                    doc.data().Class    || "empty", 
-                    doc.data().Student  || "empty", 
-                    doc.data().Meet     || "empty", 
-                    doc.data().Start    || { "seconds": 0000000000, "nanoseconds": 000000000 }, 
-                    doc.data().End      || { "seconds": 0000000000, "nanoseconds": 000000000 },
-                    );
-                    if (doc.data().status === true) {
-                        schedules.push(schedule);
-                    }
-                });
-                res.send(schedules);
-            }
+                    doc.id,
+                    doc.data().Class || "empty",
+                    doc.data().Student || "empty",
+                    doc.data().Meet || "empty",
+                    doc.data().Start || { "seconds": 0000000000, "nanoseconds": 000000000 },
+                    doc.data().End || { "seconds": 0000000000, "nanoseconds": 000000000 },
+                );
+                if (doc.data().status === true) {
+                    schedules.push(schedule);
+                }
+            });
+            res.send(schedules);
+        }
     } catch (error) {
         res.status(400).send(error.message);
     }
 };
 
 const addAdvisorsSchedules = async (req, res, next) => {
-    console.log("Advisors_add_Schedules",req.params);
-    const id                = req.params.id
-    const data              = req.body;
-    const statusSchedules   = true
-    data.status             = true
+    console.log("Advisors_add_Schedules", req.params);
+    const id = req.params.id
+    const data = req.body;
+    const statusSchedules = true
+    data.status = true
     try {
         await firestore.collection(`/Advisors/${id}/Schedules`).add(data);
         const fire = await firestore.collection("Advisors").doc(id);
-        await fire.update({statusSchedules});
+        await fire.update({ statusSchedules });
         res.send("Schedules successfuly");
     } catch (error) {
         res.status(400).send(error.message);
@@ -230,11 +231,11 @@ const addAdvisorsSchedules = async (req, res, next) => {
 
 const updatAdvisorsSchedules = async (req, res, next) => {
     console.log("Advisors_updat_Schedules");
-    const id    = req.params.id;
-    const idr   = req.params.idr;
-    console.log("params",req.params);
-    console.log("body",req.body);
-    const data  = req.body;
+    const id = req.params.id;
+    const idr = req.params.idr;
+    console.log("params", req.params);
+    console.log("body", req.body);
+    const data = req.body;
     try {
         const rev = await firestore.collection(`/Advisors/${id}/Schedules`).doc(idr);
         //await rev.update(data);
@@ -257,36 +258,36 @@ const getAdvisorsAllMyWallet = async (req, res, next) => {
         } else {
             data.forEach((doc) => {
                 const myWallet = new MyWallet(
-                    doc.data().userName    || "empty", 
-                    doc.data().TechSkills  || "empty", 
-                    doc.data().myPayment   || "empty", 
-                    );
-                    if (doc.data().status === true) {
-                        myWallets.push(myWallet);
-                    }
-                });
-                res.send(myWallets);
-            }
+                    doc.data().userName || "empty",
+                    doc.data().TechSkills || "empty",
+                    doc.data().myPayment || "empty",
+                );
+                if (doc.data().status === true) {
+                    myWallets.push(myWallet);
+                }
+            });
+            res.send(myWallets);
+        }
     } catch (error) {
         res.status(400).send(error.message);
     }
 };
 
-const addAdvisorsMyWallet= async (req, res, next) => {
-    console.log("Advisors_add_MyWallet",req.params);
-    const aid                = req.params.id
-    const uid                = req.body.id
+const addAdvisorsMyWallet = async (req, res, next) => {
+    console.log("Advisors_add_MyWallet", req.params);
+    const aid = req.params.id
+    const uid = req.body.id
     const dataAdvisor = {
-        userName : req.body.userName,
-        TechSkills : req.body.TechSkills,
-        myPayment : req.body.price,
-        status : true
+        userName: req.body.userName,
+        TechSkills: req.body.TechSkills,
+        myPayment: req.body.price,
+        status: true
     };
     const dataUser = {
-        advisorName : req.body.advisorName,
-        TechSkills : req.body.TechSkills,
-        myPurchase : req.body.price,
-        status : true
+        advisorName: req.body.advisorName,
+        TechSkills: req.body.TechSkills,
+        myPurchase: req.body.price,
+        status: true
     };
     try {
         await firestore.collection(`/Advisors/${aid}/MyWallet`).add(dataAdvisor);
@@ -294,8 +295,8 @@ const addAdvisorsMyWallet= async (req, res, next) => {
 
         const fireAdvisors = await firestore.collection("Advisors").doc(aid);
         const fireUser = await firestore.collection("User").doc(uid);
-        await fireAdvisors.update({statusMyWallet:true});
-        await fireUser.update({statusMyCart:true});
+        await fireAdvisors.update({ statusMyWallet: true });
+        await fireUser.update({ statusMyCart: true });
         res.send("MyWallet successfuly");
     } catch (error) {
         res.status(400).send(error.message);
@@ -310,7 +311,7 @@ const deleteAdvisors = async (req, res, next) => {
         await firestore.collection("Advisors").doc(id).delete();
         res.send("Advisors deleted successfuly");
     } catch (error) {
-    res.status(400).send(error.message);
+        res.status(400).send(error.message);
     }
 };
 
@@ -325,7 +326,7 @@ module.exports = {
 
     updatAdvisorsReviwers,
     updatAdvisorsSchedules,
-    
+
     getAdvisorsAllMyWallet,
     addAdvisorsMyWallet,
 
