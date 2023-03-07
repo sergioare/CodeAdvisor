f095f377078cd0982adfede47bc2adb3176e8a60
Yoel Preza Del Razo <preza_95@hotmail.com>
2023-03-07 16:56:43 -0600

    feature(ModShopping): para mergear

--- client/src/components/Modals/ModShopping.jsx
+++ client/src/components/Modals/ModShopping.jsx
@@ -5,7 +5,7 @@ import './ModShopping.scss';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { getCartItems, clearCart} from "../../redux/actions/actions"
+import { getCartItems, clearCart } from "../../redux/actions/actions"
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { getAuth } from 'firebase/auth'
@@ -13,23 +13,23 @@ import { getAuth } from 'firebase/auth'
 
 
 const ModShopping = () => {
-    
+
     const dispatch = useDispatch();
 
     //------------------- auth User --------------------------//
     const auth = getAuth();
     const idClient = auth.currentUser ? auth.currentUser.uid : null;
 
-//-------------------------- Data Source------------------------//
+    //-------------------------- Data Source------------------------//
     const productsIN = useSelector(state => state.productsInCart)
     const productsInCart = productsIN.cart?.MyCart
 
-//----------------- filter to choose the Product -------------//
-    const productStatusPending = productsInCart?.filter((s)=>s.status === "pending")
-    
+    //----------------- filter to choose the Product -------------//
+    const productStatusPending = productsInCart?.filter((s) => s.status === "pending")
+
     const { id } = useParams();
-   
-   
+
+
     useEffect(() => {
         dispatch(getCartItems(idClient));
     }, [dispatch, idClient, productStatusPending]);
@@ -43,29 +43,15 @@ const ModShopping = () => {
     //     axios.put(`https://code-advisor-back.vercel.app/User/${idClient}/MyCart/${}`, fail)
     // }
 
-    const handleRemove =(event)=>{
-        event.preventDefault()
-        axios.put(`https://code-advisor-back.vercel.app/User/ALgS5hpPocTCHveuuzNHXdqUu0w2/MyCart/2hOuoKCMJrEcEcARifku`, deleted)
-    }
-
-    // const payHandler = (event) => {
+    // const handleRemove =(event)=>{
     //     event.preventDefault()
-    //     axios.post('https://code-advisor-xi.vercel.app/payment', prod)
-    //         .then((res) =>
-    //             (window.location.href = res.data.response.body.init_point));
-    // };
+    //     axios.put(`https://code-advisor-back.vercel.app/User/ALgS5hpPocTCHveuuzNHXdqUu0w2/MyCart/2hOuoKCMJrEcEcARifku`, deleted)
+    // }
 
-    // const prod = {
-    //     // idAdvisor: product.id,
-    //     Title: "Yoel",
-    //     Quantity: 2,
-    //     Price: 500
-    // };
-    // console.log(prod)
 
-    const deleted = {
-        status: "success"
-    }
+    // const deleted = {
+    //     status: "success"
+    // }
 
     const [isOpenShop, openModalShop, closeModalShop] = useModal(false);
 
@@ -77,43 +63,51 @@ const ModShopping = () => {
                 Let´s to Shopping
 
                 {productStatusPending?.length === 0 ? null : productStatusPending?.map((p) => {
-                    
+
                     return (
-                        
+
                         <div className="itemsCart">
-                            <h2> Advisor: <span>{p.Firstname} </span> </h2> 
+                            <h2> Advisor: <span>{p.Firstname} </span> </h2>
                             <h2>Tech Skill: <span>{p.techskill}</span> </h2>
-                            <h2>Price: <span>${p.Price}.00 / hr</span> </h2> 
+                            <h2>Price: <span>${p.Price}.00 / hr</span> </h2>
                             <h2>Date: <span>{p.Month}/{p.Day}/{p.Year}</span> </h2>
                             <h2>Time: <span>{p.StartingHour}:00 hrs - {p.EndingHour}:00 hrs </span> </h2>
                             <h2>Hours: <span>{p.hours} hrs</span> </h2>
                             {/* <h2>Total: <span>{p.Price * p.hours}</span> </h2> */}
 
-                                {sumaPriceTotal += p.Price * p.hours}
+                            {sumaPriceTotal += p.Price * p.hours}
 
-                                
+
                             <div className="btnconteiner">
-                            <button onClick={        
-                                (event) => {
-                                    event.preventDefault()
-                                    axios.post('https://code-advisor-xi.vercel.app/payment', {Title: p.Firstname, Quantity: p.hours, Price: p.Price})
-                                    .then((res) =>
-                                    (window.location.href = res.data.response.body.init_point));
-                                }
-                            }> 
-                            
-                            Pay ${p.Price * p.hours} </button>
-                            
-                            
+                                <button onClick={
+                                    (event) => {
+                                        event.preventDefault()
+                                        axios.post('https://code-advisor-xi.vercel.app/payment', { Title: p.Firstname, Quantity: p.hours, Price: p.Price })
+                                            .then((res) =>
+                                                (window.location.href = res.data.response.body.init_point));
+                                    }
+                                }>
+                                    Pay ${p.Price * p.hours} </button>
                             </div>
                             <button className="btnDelete" onClick={handleRemove}> <i class="fa-solid fa-trash"></i> </button>
-                            
+
                         </div>
                     )
                 }
-                ) }
-                <p> Total Pay ${sumaPriceTotal} </p>
-                {productStatusPending?.length !== 0 ? <button className="btnClear" onClick={handlerClear}> Clear Cart</button> : null}
+                )}
+
+                <p>Subtotal : ${sumaPriceTotal}</p>
+                <button onClick={
+                    (event) => {
+                        event.preventDefault()
+                        axios.post('https://code-advisor-xi.vercel.app/payment', { Title: "CodeAdvisor", Quantity: 1, Price: sumaPriceTotal })
+                            .then((res) =>
+                                (window.location.href = res.data.response.body.init_point));
+                    }
+                }
+                > Pay all  </button>
+
+                {productStatusPending ? <button className="btnClear" onClick={handlerClear}> Clear Cart</button> : null}
 
             </Modal>
 
