008b6f919af3e9cfa16401f4d53b27c881a266c5
María Teresa Morón <mariatmd97@hotmail.com>
2023-03-07 15:51:36 -0500

    fix(server/controllers/rutas) modify nodeMailer and nchimpMailercontroller

--- server/src/controllers/nchimpMailercontroller.js
+++ server/src/controllers/nchimpMailercontroller.js
@@ -1,43 +1,45 @@
 const mailchimp = require("@mailchimp/mailchimp_marketing");
+require('dotenv').config()
+const passwor = process.env.MAILCHIMP_API_KEY
 
 mailchimp.setConfig({
-    apiKey: "fdd331113a2c27813df88f87899158e7-us14",
-    server: "us14",
-  });
+  apiKey: passwor,
+  server: "us11",
+});
 
 
-const listMembers = async (req, res, next) => {    
-    try {
-        //trae todos los que esta subcritos
-        const response = await mailchimp.lists.getListMembersInfo("cfd495c9d2");
-      console.log(response);  
-       
-        res.status(200).send(response);
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+const listMembers = async (req, res, next) => {
+  try {
+    //trae todos los que esta subcritos
+    const response = await mailchimp.lists.getListMembersInfo("0ad7923748");
+    console.log(response);
+
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
-  
-const addSuscriber = async (req, res, next) => {    
-    try {
-        //subcribe a una persona nueba 
-        const data = req.body
-        const response = await mailchimp.lists.addListMember("cfd495c9d2", {
-            email_address: data.email,
-            full_name: data.name,
-            status: "subscribed"
-          });
-          console.log(response);
-       
-        res.status(200).send(response);
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+
+const addSuscriber = async (req, res, next) => {
+  try {
+    //subcribe a una persona nueba 
+    const data = req.body
+    const response = await mailchimp.lists.addListMember("0ad7923748", {
+      email_address: data.email_address,
+      full_name: data.full_name,
+      status: "subscribed"
+    });
+    console.log(response);
+
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
-      
-
-      //finciona agregar a un contacto
-    
 
 
-    module.exports = { addSuscriber, listMembers}
+//finciona agregar a un contacto
+
+
+
+module.exports = { addSuscriber, listMembers }
