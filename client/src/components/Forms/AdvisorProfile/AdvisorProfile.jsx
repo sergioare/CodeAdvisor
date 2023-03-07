080ae8693beeec44761d61248fb48702159853e3
Sergio Arévalo <sergioarevalo301@gmail.com>
2023-03-06 19:50:14 -0500

    styles in form advisor and user

--- client/src/components/Forms/AdvisorProfile/AdvisorProfile.jsx
+++ client/src/components/Forms/AdvisorProfile/AdvisorProfile.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import './AdvisorProfile.scss'
 import { useNavigate } from "react-router-dom";
 import { PhoneInput } from "react-international-phone";
-//import "react-international-phone/style.css";
+import "react-international-phone/style.css";
 import { getTechSkills } from "../../../redux/actions/actions";
 import Swal from 'sweetalert2'
 import { getAuth } from "firebase/auth";
@@ -132,147 +132,165 @@ const AdvisorProfile = () => {
           }) => (
             <Form >
               <h1>Complete Advisor Profile</h1>
-              <Field
-                as="select"
-                name="Residence"
-                id="Residence"
-                onChange={(e) => {
-                  setcountrySelected(e.target.value);
-                  handleChange(e);
-                }}
-                // error={errors.town}
-                value={values.Residence}
-                className='input'
-              >
-                <option>Select your Country...</option>
-                {country_list.map((country) => {
-                  return (
-                    <option
-                      key={country.country_short_name}
-                      value={country.country_name}
-                    >
-                      {country.country_name}
-                    </option>
-                  );
-                })}
-              </Field>
-
-              
-              <h6>Enter your contact number</h6>
-              <div className='phone'>
-                <PhoneInput
-                  initialCountry="ar"
-                  value={phone}
-                  onChange={(phone) => setPhone(phone)}
+              <div className="residence">
+                <Field
+                  as="select"
+                  name="Residence"
+                  id="Residence"
+                  onChange={(e) => {
+                    setcountrySelected(e.target.value);
+                    handleChange(e);
+                  }}
+                  // error={errors.town}
+                  value={values.Residence}
                   className='input'
-                />
+                >
+                  <option>Select your Country...</option>
+                  {country_list.map((country) => {
+                    return (
+                      <option
+                        key={country.country_short_name}
+                        value={country.country_name}
+                      >
+                        {country.country_name}
+                      </option>
+                    );
+                  })}
+                </Field>
               </div>
 
-              {errors.Contact &&
-                touched.Contact(<p style={{ color: "red" }}>{errors.Contact}</p>)}
+              
+              <div className='phone'>
+                  <h6>Enter your contact number</h6>
+                    <PhoneInput
+                      initialCountry="ar"
+                      value={phone}
+                      onChange={(phone) => setPhone(phone)}
+                      className="wrapper-phone"
+                    />
+
+                  {errors.Contact &&
+                    touched.Contact(<p style={{ color: "red" }}>{errors.Contact}</p>)}
+              </div>
+
 
              
-              <h6>Insert your portfolio URL</h6>
-              <Field
-                type="text"
-                placeholder="URL portfolio"
-                name="portfolio"
-                id="portfolio"
-                onChange={handleChange}
-                value={values.portfolio}
-                className='input'
-              />
-         
-              <Field
-                as="select"
-                name="TechSkills"
-                id="professionselect"
-                placeholder="Select your Profession..."
-                onChange={(e) => {
-                  handleChange(e);
-                  setTechSelected([...techSelected,e.target.value]);
-                }}
-                value={values.TechSkills}
-                className='input'
-              >
-                <option>Select your Programming Language...</option>
-                {TechSkills.map( tech => { 
-                  return (
-                    <option key={tech.name} value={tech.name}>
-                      {tech.name}
-                    </option>
-                  );
-                })}
-              </Field>
+             <div className="portfolio">
+                  <h6>Insert your portfolio URL</h6>
+                  <Field
+                    type="text"
+                    placeholder="URL portfolio"
+                    name="portfolio"
+                    id="portfolio"
+                    onChange={handleChange}
+                    value={values.portfolio}
+                    className='input'
+                  />
+             </div>
 
-              <h6 className='h6form'>Tell people about your work</h6>
-              <Field
-                as="textarea"
-                name="About"
-                onChange={handleChange}
-                placeholder="Write a brief description about your work..."
-                className='textarea'
-                value={values.About}
-              />
-
-              <h6>Set your price per hour</h6>
-              <Field
-                type="text"
-                placeholder="USD/hr"
-                name="Price"
-                id="Price"
-                onChange={handleChange}
-                value={values.Price}
-                className='input'
-              />
-
-              <Field
-                as="select"
-                name="Specialty"
-                id="Specialty"
-                placeholder="Select your specialty"
-                onChange={(e) => {
-                  handleChange(e);
-                  setSpecialty([...Specialty,e.target.value]);
-                }}
-                value={values.Specialty}
-                className='input'
-              >
-                <option>Select your Specialties</option>
-                {Specialties.map( tech => { 
-                  return (
-                    <option key={tech.name} value={tech.name}>
-                      {tech.name}
-                    </option>
-                  );
-                })}
-              </Field>
-
-
-              <h6 >Upload your profile image</h6>
-              <input
-                    type="file"
-                    name="photo"
-                    id="photo"
-                    className="shadow"
-                    onChange={handleImage}
-                />
-
-              <h6 >Upload your Curriculum vitae</h6>
-              <input
-                    type="file"
-                    name="cv"
-                    id="cv"
-                    className="shadow"
-                    onChange={handleCV}
-                />
              
-              {errors.image &&
-                touched.image(<p style={{ color: "red" }}>{errors.image}</p>)} 
+                            
+              <div className="price">
+                  <h6>Set your price per hour</h6>
+                  <Field
+                    type="text"
+                    placeholder="USD/hr"
+                    name="Price"
+                    id="Price"
+                    onChange={handleChange}
+                    value={values.Price}
+                    className='input'
+                  />
+              </div>
 
-              <button className='btn' type="submit" onClick={showAlert}>
-                Send
-              </button>
+              <div className="specialty">
+                  <Field
+                    as="select"
+                    name="Specialty"
+                    id="Specialty"
+                    placeholder="Select your specialty"
+                    onChange={(e) => {
+                      handleChange(e);
+                      setSpecialty([...Specialty,e.target.value]);
+                    }}
+                    value={values.Specialty}
+                    className='input'
+                  >
+                    <option>Select your Specialties</option>
+                    {Specialties.map( tech => { 
+                      return (
+                        <option key={tech.name} value={tech.name}>
+                          {tech.name}
+                        </option>
+                      );
+                    })}
+                  </Field>
+              </div>
+              
+              <div className="second-part">
+                  <div className="tech">
+                  <Field
+                    as="select"
+                    name="TechSkills"
+                    id="professionselect"
+                    placeholder="Select your Profession..."
+                    onChange={(e) => {
+                      handleChange(e);
+                      setTechSelected([...techSelected,e.target.value]);
+                    }}
+                    value={values.TechSkills}
+                    className='input'
+                  >
+                    <option>Select your Programming Language...</option>
+                    {TechSkills.map( tech => { 
+                      return (
+                        <option key={tech.name} value={tech.name}>
+                          {tech.name}
+                        </option>
+                      );
+                    })}
+                  </Field>
+                </div>
+
+                  <div className="imgProf">
+                      <h6 >Upload your profile image</h6>
+                      <input
+                            type="file"
+                            name="photo"
+                            id="photo"
+                            className="shadow"
+                            onChange={handleImage}
+                        />
+                  </div>
+                  <div className="about">
+                      <h6 className='h6form'>Tell people about your work</h6>
+                      <Field
+                        as="textarea"
+                        name="About"
+                        onChange={handleChange}
+                        placeholder="Write a brief description about your work..."
+                        className='textarea'
+                        value={values.About}
+                      />
+                  </div>
+
+                    
+                  <div className="cv">
+                      <h6 >Upload your Curriculum vitae</h6>
+                      <input
+                            type="file"
+                            name="cv"
+                            id="cv"
+                            className="shadow"
+                            onChange={handleCV}
+                        />
+                  </div>
+                  <button className='btn' type="submit" onClick={showAlert}>
+                    Send
+                  </button>
+              </div>
+              
+             
 
               {responseServer && <div>{responseServer}</div>}
             </Form>
